da1dacd7e620b16878940d150d754330fe98e868
Doktatech <Doktatech2@gmail.com>
2019-02-25 07:36:01 +0300

    add Read.me file

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
 function completeQuiz(){
-  var answersCorrect=0;
+  var rewel =0 ;
   for(var i = 1; i<=5;i++){
-    var radios= document.getElementsByName('name');
-    for (var z=0; z<radios.length; z++){
-      var radio =radios[z];
-      if(radio.value=== "correct" && radio.checked){
-
+    var radios= document.getElementsByName('question'+i);
+    for (var z=0; z< radios.length; z++){
+      var radio = radios[z];
+      if(radio.value == "correct" && radio.checked){
+        rewel++;
       }
     }
   }
-      alert("correct Responses: "+answersCorrect);
+      alert("You got" + " "+ rewel + " "+ "correct");
 }
