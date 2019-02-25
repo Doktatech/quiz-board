ce0a2f096e7cd0503ed92b4886d3aada912516a7
Doktatech <Doktatech2@gmail.com>
2019-02-25 14:18:59 +0300

    Modify the remarks in js

--- js/scripts.js
+++ js/scripts.js
@@ -8,16 +8,13 @@ $(document).ready(function(){
    var question5 = parseInt($("input:radio[name=question5]:checked").val());
    var total = (question1+question2+question3+question4+question5);
     var percentageScore = ((total / 5) * 100);
-      alert("You have scored " + percentageScore + "%");
-     //document.getElementById('score-results').innerHTML="You have scored " + percentageScore + "%";
-   //$("#score-results").append("You have scored " + percentageScore + "%");
-
+      $("#score-results").text("You have scored " + percentageScore + "%");
    if (percentageScore  > 80) {
-    alert ("Congratulations,You have excellently Passed!");
+      $("#remark").text("Congratulations,You have excellently Passed!");
   }else if ((percentageScore  > 50) && (percentageScore  < 79)) {
-    alert ("Fair score.You are almost there");
+      $("#remark").text("Fair score.You are almost there");
    }else {
-    alert ("Failed. Consider retaking the course!");
+      $("#remark").text("Failed. Consider retaking the course!");
    }
  });
 });
